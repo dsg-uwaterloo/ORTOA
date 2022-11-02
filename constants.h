42384f078842cd9dd7231c33484f95fdf271a453
ySteinhart1 <43654693+ySteinhart1@users.noreply.github.com>
2022-11-02 09:45:29 -0700

    Update constants.h
    Former-commit-id: c9617589fef766cfc5d3c47b2f191a21d1ed8606

--- constants.h
+++ constants.h
@@ -3,11 +3,11 @@
 
 #define VALUE_SIZE 160
 #define KEY_SIZE 10
-#define PROXY_IP "54.219.133.68"
+#define PROXY_IP "localhost"
 #define SERVER_IP "localhost"
 #define SERVER_PORT 9090
 #define PROXY_PORT 3000
-#define KEY_MAX 1048576
+#define KEY_MAX 100000
 #define DATA_FILE "./OpScure.data"
 
 #endif
