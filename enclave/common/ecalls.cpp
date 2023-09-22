ec9c0380a1db788b142759db98e9ab59ae517071
sujaya <maiyya.sujaya@gmail.com>
2023-09-22 17:54:36 -0400

    Adding a MVP of ORTOA using TEEs

---
+++ enclave/common/ecalls.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include <iostream>
+#include <string>
+
+#include "../crypto/encryption_engine.h"
+#include "ortoa_t.h"
+#include "shared.h"
+
+using namespace std;
+
+void access_data(const char* opConst, size_t opConstSize,
+                const char*  inVal, size_t inSize, unsigned char* cipher_text, size_t* outSize) {
+    int c, val, out;
+    string cStr = opConst;
+    encryption_engine encryption_engine_;
+    string inStr((const char *)inVal, inSize);
+    string valStr = encryption_engine_.decryptNonDeterministic(inStr);
+    c = stoi(cStr);
+    val = stoi(valStr);
+    cout << "In enclave: Decryption val is: " << valStr << endl;
+    // TODO: If c == 0: read else write
+    out = c + val;
+    *outSize = (size_t)encryption_engine_.encryptNonDeterministic(to_string(out), cipher_text);
+}
---
+++ host/redis.cpp
@@ -0,0 +1,19 @@
+#include "redis.h"
+
+using namespace sw::redis;
+
+redisCli::redisCli() {
+}
+
+void redisCli::reconnect(){
+    this->redisConn = sw::redis::Redis("tcp://127.0.0.1:6379");
+}
+
+std::string redisCli::get(const std::string &key){
+    auto reply = this->redisConn.get(key);
+    return *reply;
+}
+
+void redisCli::put(const std::string &key, const std::string &value){
+    this->redisConn.set(key, value);
+}
