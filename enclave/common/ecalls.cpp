58104910b059585cd9758c07beed77667a24fe31
adrian <a4davila@uwaterloo.ca>
2023-10-16 21:03:18 -0400

    Add and use logger

--- enclave/common/ecalls.cpp
+++ enclave/common/ecalls.cpp
@@ -2,24 +2,25 @@
 // Licensed under the MIT License.
 
 #include <openenclave/enclave.h>
+
 #include <iostream>
 #include <string>
 
 #include "../crypto/encryption_engine.h"
 #include "ortoa_t.h"
 #include "shared.h"
-
 using namespace std;
 
 void access_data(const char* opConst, size_t opConstSize,
-                const char*  inVal, size_t inSize, unsigned char* cipher_text, size_t* outSize) {
+                 const char* inVal, size_t inSize, unsigned char* cipher_text, size_t* outSize) {
     int c, val, out;
     string cStr = opConst;
     encryption_engine encryption_engine_;
-    string inStr((const char *)inVal, inSize);
+    string inStr((const char*)inVal, inSize);
     string valStr = encryption_engine_.decryptNonDeterministic(inStr);
     c = stoi(cStr);
     val = stoi(valStr);
+
     cout << "In enclave: Decryption val is: " << valStr << endl;
     // TODO: If c == 0: read else write
     out = c + val;
--- host/host.cpp
+++ host/host.cpp
@@ -7,25 +7,24 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <vector>
-#include "../shared.h"
-#include "redis.h"
 
+#include "../shared.h"
 #include "ortoa_u.h"
+#include "redis.h"
+#include "spdlog/spdlog.h"
 
 using namespace std;
 
 oe_enclave_t* enclave = NULL;
 
-bool check_simulate_opt(int* argc, const char* argv[])
-{
-    for (int i = 0; i < *argc; i++)
-    {
-        if (strcmp(argv[i], "--simulate") == 0)
-        {
+bool check_simulate_opt(int* argc, const char* argv[]) {
+    for (int i = 0; i < *argc; i++) {
+        if (strcmp(argv[i], "--simulate") == 0) {
             cout << "Running in simulation mode" << endl;
             memmove(&argv[i], &argv[i + 1], (*argc - i) * sizeof(char*));
             (*argc)--;
@@ -35,8 +34,7 @@ bool check_simulate_opt(int* argc, const char* argv[])
     return false;
 }
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
     oe_result_t result;
     int ret = 0, res;
     uint32_t flags = OE_ENCLAVE_FLAG_DEBUG;
@@ -47,44 +45,43 @@ int main(int argc, const char* argv[])
     string val;
     string opConst = "1";
     string updateVal;
-    char * copy;
+    char* copy;
 
-    if (check_simulate_opt(&argc, argv))
-    {
+    if (check_simulate_opt(&argc, argv)) {
         flags |= OE_ENCLAVE_FLAG_SIMULATE;
     }
 
-    cout << "Host: enter main" << endl;
-    if (argc != 2)
-    {
-        cerr << "Usage: " << argv[0]
-             << " enclave_image_path [ --simulate  ]" << endl;
+    spdlog::info("Host | enter main");
+
+    if (argc != 2) {
+        spdlog::error("Usage: {0} enclave_image_path [ --simulate ]", argv[0]);
         return 1;
     }
 
-    cout << "Host: create enclave for image:" << argv[1] << endl;
+    spdlog::info("Host | create enclave for image: {0}", argv[1]);
+
     result = oe_create_ortoa_enclave(
         argv[1], OE_ENCLAVE_TYPE_SGX, flags, NULL, 0, &enclave);
-    if (result != OE_OK)
-    {
-        cerr << "oe_create_ortoa_enclave() failed with " << argv[0]
-             << " " << result << endl;
+
+    if (result != OE_OK) {
+        spdlog::error("oe_create_ortoa_enclave() failed with {0} {1}", argv[0], result);
         ret = 1;
     }
+
     val = rd.get("1");
-    cout << "Host: Redis get: " << val << " with len " << val.length() << endl;
+
+    spdlog::info("Host | Redis get: {0} with len {1}", val, val.length());
     result = access_data(enclave, opConst.c_str(), opConst.length(), val.c_str(), val.length(), out, &outLen);
-    if (result == OE_OK)
-    {
-        string updatedVal((const char *)out, outLen);
-        cout << "Host: Output of access_data " << updatedVal << " with len " << outLen << endl;
+    if (result == OE_OK) {
+        string updatedVal((const char*)out, outLen);
+        spdlog::info("Host | Output of access_data {0} with len {1}", updatedVal, outLen);
         rd.reconnect();
         rd.put("1", updatedVal);
     }
 
 exit:
-    cout << "Host: terminate the enclave" << endl;
-    cout << "Host: Sample completed successfully." << endl;
+    spdlog::info("Host | terminate the enclave");
+    spdlog::info("Host | Sample completed successfully.");
     if (enclave)
         oe_terminate_enclave(enclave);
     return ret;
