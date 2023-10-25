2ecc0e7682aaf3c1fee8ae324657702632a0cdcb
Jason Du <jason.moshi.du@gmail.com>
2023-10-24 20:50:57 -0400

    Run formatting and linting

--- client/client.cpp
+++ client/client.cpp
@@ -2,17 +2,17 @@
 #include <fstream>
 #include <iostream>
 #include <numeric>
-#include <sstream>
 #include <sodium.h>
+#include <sstream>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
 
-#include "client_utils.h"
 #include "../constants/constants.h"
 #include "../crypto/encryption_engine.h"
 #include "../gen-cpp/RPC.h"
 #include "../host/redis.h"
+#include "client_utils.h"
 
 using namespace std::chrono;
 using namespace apache::thrift;
@@ -20,115 +20,122 @@ using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
 class ClientHandler {
- private:
-	std::ifstream seed_data; 
-	bool init_db = false;
-	int num_clients = 16;
-	float p_get = 0.5;
+  private:
+    std::ifstream seed_data;
+    bool init_db = false;
+    int num_clients = 16;
+    float p_get = 0.5;
 
-	std::vector<float> latencies;
+    std::vector<float> latencies;
 
- public:
-	ClientHandler(int argc, char *argv[]) {
-		parseArgs(argc, argv, seed_data, init_db, num_clients, p_get);
-	}
+  public:
+    ClientHandler(int argc, char *argv[]) {
+        parseArgs(argc, argv, seed_data, init_db, num_clients, p_get);
+    }
 
-	void start() {
-		if (init_db) {
-			initDB();
-		} else {
-			runThreaded();
-		}
-	}
+    void start() {
+        if (init_db) {
+            initDB();
+        } else {
+            runThreaded();
+        }
+    }
 
-	void initDB() {
-		redisCli rd;
-		auto pipeline = rd.pipe();
+    void initDB() {
+        redisCli rd;
+        auto pipeline = rd.pipe();
 
-		// If seed data exists, initialize the db with seed data
-		if (seed_data.is_open()) {
-			std::string line;
-			while (std::getline(seed_data, line)) {
-				Operation op = getSeedOperation(line);
-				pipeline.set(op.key, op.value);
-			}
-		} 
-		// If seed data does not exist, initialize db with key from 0 to KEY_MAX
-		else {
-			for (int i = 0; i < KEY_MAX; ++i) {
-				std::string value = std::to_string(rand() % VAL_MAX);
-				pipeline.set(std::to_string(i), clientEncrypt(value));
-			}
-		}
+        // If seed data exists, initialize the db with seed data
+        if (seed_data.is_open()) {
+            std::string line;
+            while (std::getline(seed_data, line)) {
+                Operation op = getSeedOperation(line);
+                pipeline.set(op.key, op.value);
+            }
+        }
+        // If seed data does not exist, initialize db with key from 0 to KEY_MAX
+        else {
+            for (int i = 0; i < KEY_MAX; ++i) {
+                std::string value = std::to_string(rand() % VAL_MAX);
+                pipeline.set(std::to_string(i), clientEncrypt(value));
+            }
+        }
 
-		pipeline.exec();
-	}
+        pipeline.exec();
+    }
 
-	void runThreaded() {
-		std::vector<std::thread> threads;
-		for (int i = 0; i < num_clients; i++) {
-			threads.push_back(std::thread(&ClientHandler::run, this));
-		}
+    void runThreaded() {
+        std::vector<std::thread> threads;
+        for (int i = 0; i < num_clients; i++) {
+            threads.push_back(std::thread(&ClientHandler::run, this));
+        }
 
-		// Wait for all threads to finish
-		for (std::thread& thread : threads) thread.join();
-		
-		getAveLatency();
-	}
+        // Wait for all threads to finish
+        for (std::thread &thread : threads)
+            thread.join();
 
-	void run() {
-		auto socket = std::make_shared<TSocket>(HOST_IP, HOST_PORT);
-		auto transport = std::make_shared<TBufferedTransport>(socket);
-		auto protocol = std::make_shared<TBinaryProtocol>(transport);
-		RPCClient client(protocol);
+        getAveLatency();
+    }
 
-		transport->open();
+    void run() {
+        auto socket = std::make_shared<TSocket>(HOST_IP, HOST_PORT);
+        auto transport = std::make_shared<TBufferedTransport>(socket);
+        auto protocol = std::make_shared<TBinaryProtocol>(transport);
+        RPCClient client(protocol);
 
-		std::string val;
-		// If seed data exists, run the client with data
-		if (seed_data.is_open()) {
-			std::string line;
-			while (readFile(seed_data, line)) {
-				Operation op = getSeedOperation(line);
-				auto start = high_resolution_clock::now();
-				client.access(val, op);
-				auto end = high_resolution_clock::now();
-				latencies.push_back(duration_cast<microseconds>(end - start).count());
-			}
-		} 
-		// If seed data does not exist, run client on random values
-		else {
-			for (int i = 0; i < 1000; ++i) {
-				Operation op = genRandOperation(p_get);
-				auto start = high_resolution_clock::now();
-				client.access(val, op);
-				auto end = high_resolution_clock::now();
-				latencies.push_back(duration_cast<microseconds>(end - start).count());
-			}
-		}
+        transport->open();
 
-		transport->close();
-	}
+        std::string val;
+        // If seed data exists, run the client with data
+        if (seed_data.is_open()) {
+            std::string line;
+            while (readFile(seed_data, line)) {
+                Operation op = getSeedOperation(line);
+                auto start = high_resolution_clock::now();
+                client.access(val, op);
+                auto end = high_resolution_clock::now();
+                latencies.push_back(
+                    duration_cast<microseconds>(end - start).count());
+            }
+        }
+        // If seed data does not exist, run client on random values
+        else {
+            for (int i = 0; i < 1000; ++i) {
+                Operation op = genRandOperation(p_get);
+                auto start = high_resolution_clock::now();
+                client.access(val, op);
+                auto end = high_resolution_clock::now();
+                latencies.push_back(
+                    duration_cast<microseconds>(end - start).count());
+            }
+        }
 
-	void getAveLatency() {
-		std::cout << "[Client]: Data access complete, average latency: " << std::accumulate(latencies.begin(), latencies.end(), 0.0) / latencies.size() << " microseconds" << std::endl;
-	}
+        transport->close();
+    }
+
+    void getAveLatency() {
+        std::cout << "[Client]: Data access complete, average latency: "
+                  << std::accumulate(latencies.begin(), latencies.end(), 0.0) /
+                         latencies.size()
+                  << " microseconds" << std::endl;
+    }
 };
 
-
 int main(int argc, char *argv[]) {
-  auto start = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
 
-	ClientHandler client(argc, argv);
+    ClientHandler client(argc, argv);
 
-	try {
-		client.start();
-	} catch (std::invalid_argument& err) {
-		std::cerr << "ERROR: " << err.what() << std::endl;
-	} catch (TException& err) {
-		std::cerr << "ERROR: " << err.what() << std::endl;
-  }
+    try {
+        client.start();
+    } catch (std::invalid_argument &err) {
+        std::cerr << "ERROR: " << err.what() << std::endl;
+    } catch (TException &err) {
+        std::cerr << "ERROR: " << err.what() << std::endl;
+    }
 
-	auto end = high_resolution_clock::now();
-  std::cout << "[main]: Entire program finished in " << duration_cast<microseconds>(end - start).count() << " microseconds" << std::endl;
+    auto end = high_resolution_clock::now();
+    std::cout << "[main]: Entire program finished in "
+              << duration_cast<microseconds>(end - start).count()
+              << " microseconds" << std::endl;
 }
--- client/client_utils.h
+++ client/client_utils.h
@@ -1,20 +1,21 @@
 #include <fstream>
 #include <iostream>
 #include <mutex>
-#include <sstream>
 #include <sodium.h>
+#include <sstream>
 
 #include "../constants/constants.h"
 #include "../crypto/encryption_engine.h"
 #include "../gen-cpp/RPC.h"
 #include "../host/redis.h"
 
-void parseArgs(int argc, char *argv[], std::ifstream &seed, bool &init_db, int &num_clients, float &p_get);
+void parseArgs(int argc, char *argv[], std::ifstream &seed, bool &init_db,
+               int &num_clients, float &p_get);
 
 Operation genRandOperation(int p_get);
 
-Operation getSeedOperation(std::string& line);
+Operation getSeedOperation(std::string &line);
 
-std::istream& readFile(std::ifstream &seed_data, std::string &line);
+std::istream &readFile(std::ifstream &seed_data, std::string &line);
 
 std::string clientEncrypt(const std::string &value);
