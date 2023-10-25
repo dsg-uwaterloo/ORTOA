bdb2c5ffd245c927f0c4a3385c65a3c3a55ef565
adrian <a4davila@uwaterloo.ca>
2023-10-25 12:35:46 -0400

    add scripts to run ORTOA
    Former-commit-id: 677d71a639b32e52092e4cc56aa46b91cd870d30

--- scripts/ortoa-lib.sh
+++ scripts/ortoa-lib.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # always top-level even in submodule (TODO: bug if more than one submodule deep)
 export REPO_ROOT=$(cd ${SCRIPT_DIR} && git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
-
+export BUILD_DIR=${REPO_ROOT}/build
 
 ############################################
 # Help
@@ -25,12 +25,20 @@ ortoa-help() {
 ortoa-lib: a collection of bash functions to ease development
 -------------------------------------------------------------
 
+    Running ORTOA:
+        ortoa-client-run: ----------- Run the ORTOA client
+        ortoa-simulate: ------------- Run ORTOA in simulation mode
+    
+    Data Generation:
+        ortoa-generate-seed: -------- Seed Data Generation script for ORTOA-tee
+        ortoa-generate-operations: -- Operation Generation script for ORTOA-tee 
+
     Formatters:
-        ortoa-clang-format: ------- check staged C++ projects for formatting issues using git-clang-format
-        ortoa-clang-format-all: --- check all C++ projects for formatting issues
+        ortoa-clang-format: --------- Check staged C++ files for formatting issues
+        ortoa-clang-format-all: ----- Check all C++ projects for formatting issues
 
     Other:
-        ortoa-help: --------------- prints this help message
+        ortoa-help: ----------------- Prints this help message
 
 Happy developing!
 _EOF_
@@ -38,6 +46,50 @@ _EOF_
     [[ ${#} -eq 0 ]]
 }
 
+
+############################################
+# Running ORTOA
+############################################
+
+ortoa-client-run() {
+    local HELP="""\
+Run the ORTOA client
+
+Syntax: ortoa-client-run [-h]
+----------------------------------------------
+    -h                  Print this help message
+"""
+    OPTIND=1
+    while getopts ":h" option; do
+        case "${option}" in
+            h) echo "${HELP}"; return 0 ;;
+        esac
+    done
+
+
+    ${BUILD_DIR}/client/client "${@}"
+}
+export -f ortoa-client-run
+
+
+ortoa-simulate() {
+    local HELP="""\
+Run ORTOA in sumulate mode
+
+Syntax: ortoa-simulate [-h]
+----------------------------------------------
+    -h                  Print this help message
+"""
+    OPTIND=1
+    while getopts ":h" option; do
+        case "${option}" in
+            h) echo "${HELP}"; return 0 ;;
+        esac
+    done
+
+    ${BUILD_DIR}/host/ortoa-host ${BUILD_DIR}/enclave/ortoa-enc.signed --simulate
+}
+export -f ortoa-simulate
 
 ############################################
 # Formatting and linting
