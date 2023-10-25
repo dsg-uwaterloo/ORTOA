6949de0ce13b3587b73449748e17d24ba5c7f460
Jason Du <78238426+jasondu7297@users.noreply.github.com>
2023-10-25 16:01:14 -0400

    Initialize Database with Seed Data (#20)
    * Initial design of init db
    
    * Implement flag parsing and init db
    
    * Run formatting and linting
    
    * Update seed parsing
    
    * Run formatting and linting
    
    * Remove VERBATIM from CMake file
    
    Former-commit-id: b72a902b7073cc22293b77dbc54e57edaf4d9fc8

--- client/client_utils.h
+++ client/client_utils.h
@@ -1,18 +1,21 @@
 #include <fstream>
 #include <iostream>
 #include <mutex>
-#include <sstream>
 #include <sodium.h>
+#include <sstream>
 
 #include "../constants/constants.h"
 #include "../crypto/encryption_engine.h"
 #include "../gen-cpp/RPC.h"
 #include "../host/redis.h"
 
-Operation genRandOperation();
+void parseArgs(int argc, char *argv[], std::ifstream &seed, bool &init_db,
+               int &num_clients, float &p_get);
 
-Operation getSeedOperation(std::string& line);
+Operation genRandOperation(int p_get);
 
-std::istream& readFile(std::ifstream &seed_data, std::string &line);
+Operation getSeedOperation(std::string &line);
+
+std::istream &readFile(std::ifstream &seed_data, std::string &line);
 
 std::string clientEncrypt(const std::string &value);
