1f0d44f94ddc3c86713dff9db5738b6e23a10c08
adrian <a4davila@uwaterloo.ca>
2023-10-30 21:40:35 -0400

    testing deserialization of ByteSizeGeneratorConfig
    Former-commit-id: 3d9689aacdc57bfad4d36cea13668f8f90a152ab

--- benchmark/test/interface/test_experiment_yaml.py
+++ benchmark/test/interface/test_experiment_yaml.py
@@ -1,7 +1,51 @@
+import yaml
+from pathlib import Path
+from typing import Any
+
+from benchmark.interface.data import ByteSizeGenerationConfig
+from benchmark.interface.parameter import (
+    IntegerParameter,
+    IntegerIncrementRange,
+    IntegerMultiplyRange,
+)
+
+
 def test_data_generation_config():
+    def get_path(filename: str) -> Path:
+        return Path(f"benchmark/test/interface/test_files/{filename}.yaml")
+
+    def load_yaml(file_path: Path) -> Any:
+        with open(file_path, "r") as f:
+            loaded_yaml = yaml.safe_load(f)
+        return loaded_yaml
+
     # assert can deserialize ByteSizeGenerator
+    bsg_static_int_yaml = load_yaml(get_path("byte_size_datagen_int"))
+    config = ByteSizeGenerationConfig.model_validate(bsg_static_int_yaml)
+    assert config.generator == "ByteSizeGenerator"
+    assert isinstance(config.generator, IntegerParameter)
+    assert isinstance(config.generator.value, int)
+    assert config.generator.value == 160
+
+    bsg_int_increment_range_yaml = load_yaml(get_path("byte_size_datagen_int_range"))
+    config = ByteSizeGenerationConfig.model_validate(bsg_int_increment_range_yaml)
+    assert config.generator == "ByteSizeGenerator"
+    assert isinstance(config.generator, IntegerIncrementRange)
+    assert config.n_bytes.type == "int"
+    assert config.n_bytes.minimum == 100
+    assert config.n_bytes.maximum == 500
+    assert config.n_bytes.step == 20
+
+    bsg_int_multiply_range_yaml = load_yaml(get_path("byte_size_datagen_int_multiple"))
+    config = ByteSizeGenerationConfig.model_validate(bsg_int_multiply_range_yaml)
+    assert config.generator == "ByteSizeGenerator"
+    assert isinstance(config.generator, IntegerMultiplyRange)
+    assert config.n_bytes.type == "int"
+    assert config.n_bytes.minimum == 100
+    assert config.n_bytes.maximum == 500
+    assert config.n_bytes.multiplier == 2
+
     # assert can deserialize RandomIntegerGenerator
-    pass
 
 
 def test_flag_deserialization():
