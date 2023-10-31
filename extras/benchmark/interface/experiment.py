8abc940bca11c15b20136796bbb66f6d8b62cf47
adrian <a4davila@uwaterloo.ca>
2023-10-31 15:42:41 -0400

    fix python imports in benchmarking
    Former-commit-id: 8a379db546bb07a2aa11e6592991afd451047cf4

--- extras/benchmark/interface/experiment.py
+++ extras/benchmark/interface/experiment.py
@@ -6,9 +6,9 @@ from typing_extensions import Self
 
 from pydantic import BaseModel, Field
 
-from benchmark.interface.flags import AnnotatedClientFlag, AnnotatedHostFlag
-from benchmark.interface.data import DataGenerationConfigBase, DataGenConfig
-from benchmark.infrastucture.experiment_collection import ExperimentPath
+from extras.benchmark.interface.flags import AnnotatedClientFlag, AnnotatedHostFlag
+from extras.benchmark.interface.data import DataGenerationConfigBase, DataGenConfig
+from extras.benchmark.infrastucture.experiment_collection import ExperimentPath
 
 FlagT = TypeVar("FlagT", bound=Union[AnnotatedClientFlag, AnnotatedHostFlag])
 
