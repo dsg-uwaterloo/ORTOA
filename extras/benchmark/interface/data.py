b40a29ae943225ecdf545f9e76510fbb699f1fe9
Adrian Davila <akdavila03@gmail.com>
2023-11-03 19:40:16 -0400

    data generation functionality in benchmarking
    Former-commit-id: 3ba07b61f1cda4886f2ddad64cc26a1d58812617

--- extras/benchmark/interface/data.py
+++ extras/benchmark/interface/data.py
@@ -1,4 +1,4 @@
-from typing import Union, Literal, Tuple
+from typing import Any, Union, Literal, Tuple, Optional
 from typing_extensions import Annotated
 from pydantic import BaseModel, Field
 from abc import ABC, abstractmethod
@@ -8,6 +8,11 @@ from extras.benchmark.interface.parameter import (
     IntegerMultiplyRange,
 )
 from pathlib import Path
+
+from extras.data_generation.generate_seed_data import generate_data
+from extras.data_generation.generate_sample_operations import generate_operations
+from extras.data_generation.generators.key_generator import SequentialIntKeyGenerator
+from extras.data_generation.generators.value_generator import RandomIntegerGenerator
 
 
 class DataGenerationConfigBase(BaseModel, ABC):
@@ -29,17 +34,53 @@ class ByteSizeGenerationConfig(DataGenerationConfigBase):
     generator: Literal["ByteSizeGenerator"]
     n_bytes: IntegerParameter
 
-    def generate_files(self, output_dir: Path) -> Tuple[Path, Path]:
+    def _generate_seed(self, output_file: Path) -> Path:
         raise NotImplementedError
+
+    def _generate_operations(self, seed_file: Path, output_file: Path) -> Path:
+        raise NotImplementedError
+
+    def generate_files(self, output_dir: Path) -> Tuple[Path, Path]:
+        seed = self._generate_seed(output_dir / "seed.csv")
+        operations = self._generate_operations(seed, output_dir / "operations.csv")
+
+        return seed, operations
 
 
 class RandomIntegerGenerationConfig(DataGenerationConfigBase):
     generator: Literal["RandomIntegerGenerator"]
-    minimum: int = 0
-    maximum: int = 100000
+    minimum: int
+    maximum: int
+
+    def _generate_seed(self, output_file: Path) -> Path:
+        key_generator = SequentialIntKeyGenerator()
+        value_generator = RandomIntegerGenerator(
+            min_val=self.minimum, max_val=self.maximum
+        )
+        generate_data(
+            key_generator=key_generator,
+            value_generator=value_generator,
+            num_data_points=self.seed_size,
+            output_file=output_file,
+        )
+
+    def _generate_operations(self, seed_file: Path, output_file: Path) -> Path:
+        value_generator = RandomIntegerGenerator(
+            min_val=self.minimum, max_val=self.maximum
+        )
+        generate_operations(
+            num_operations=self.num_operations,
+            input_file=seed_file,
+            output_file=output_file,
+            p_get=0.5,
+            value_generator=value_generator,
+        )
 
     def generate_files(self, output_dir: Path) -> Tuple[Path, Path]:
-        raise NotImplementedError
+        seed = self._generate_seed(output_dir / "seed.csv")
+        operations = self._generate_operations(seed, output_dir / "operations.csv")
+
+        return seed, operations
 
 
 DataGenConfig = Annotated[
