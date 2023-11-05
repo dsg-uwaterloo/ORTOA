1a497a0fbf9196e00aff931caf531ce79cdb804f
Adrian Davila <akdavila03@gmail.com>
2023-11-05 18:16:43 -0500

    add benchmarking script help text to `ortoa-lib.sh`

--- scripts/ortoa-lib.sh
+++ scripts/ortoa-lib.sh
@@ -104,7 +104,22 @@ Syntax: ortoa-simulate [-h]
 
 ortoa-benchmark() {
     local HELP="""\
+usage: main.py [-h] -e EXPERIMENTS [EXPERIMENTS ...] [-d EXPERIMENT_DIRS [EXPERIMENT_DIRS ...]] [-w WORKING_DIR] [-m MAX_PROCESSES]
 
+options:
+  -h, --help            show this help message and exit
+  -w WORKING_DIR, --working-dir WORKING_DIR
+                        Directory to use as base for experiment directory tree (default: /Users/adrian/projects/ORTOA/benchmark-2023-11-05)
+  -m MAX_PROCESSES, --max-processes MAX_PROCESSES
+                        Maximum number of processes to use when running experiments (default: None)
+
+Experiments:
+  Options to control experiments selected for compilation
+
+  -e EXPERIMENTS [EXPERIMENTS ...], --experiments EXPERIMENTS [EXPERIMENTS ...]
+                        List of experiments to compile (experiment name should match zoo object)
+  -d EXPERIMENT_DIRS [EXPERIMENT_DIRS ...], --experiment-dirs EXPERIMENT_DIRS [EXPERIMENT_DIRS ...]
+                        List of local directories to use for experiment files
 """
     python3 "${REPO_ROOT}/extras/ortoa/benchmark/infrastucture/main.py" "${@}"
 }
