3bbf4c3602fc881d774817b2125647b6ac302d36
adrian <a4davila@uwaterloo.ca>
2023-12-16 16:53:42 -0500

    libstorage

--- src/client/client_utils.h
+++ src/client/client_utils.h
@@ -7,7 +7,7 @@
 #include "../constants/constants.h"
 #include "../crypto/encryption_engine.h"
 #include "../gen-cpp/RPC.h"
-#include "../host/redis.h"
+#include "redis.h"
 
 struct ClientConfig {
     std::ifstream seed_data;
