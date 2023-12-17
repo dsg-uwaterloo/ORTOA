c5b1c8a24f25b81148e7ea9d65e53416637729eb
Adrian Davila <a4davila@uwaterloo.ca>
2023-12-17 13:42:41 -0500

    secondary y axis label (#69)
    Former-commit-id: e079dba837abf39c18932c7fa1f182bd1780ca1e

--- extras/ortoa/benchmark/infrastucture/stats_collection.py
+++ extras/ortoa/benchmark/infrastucture/stats_collection.py
@@ -20,7 +20,7 @@ class Stats(BaseModel):
     def _graph_threads_vs_latency(self, dir: Path) -> None:
         df = self.raw_df.sort_values(by=['nthreads'], ascending=True)
         ax = df.plot.bar(x="nthreads", y="average_latency")
-        ax.set_ylabel("Latency (unit)")
+        ax.set_ylabel("Latency (μs)")
         fig = ax.get_figure()
         fig.savefig(dir / "threads_vs_latency.pdf")
 
@@ -33,6 +33,7 @@ class Stats(BaseModel):
             kind="bar",
         )
         ax.set_ylabel("Throughput (ops/s)")
+        ax.right_ax.set_ylabel("Latency (μs)")
         fig = ax.get_figure()
         fig.savefig(dir / "threading_effects.pdf")
 
@@ -44,6 +45,7 @@ class Stats(BaseModel):
             secondary_y=["average_latency"],
         )
         ax.set_ylabel("Throughput (ops/s)")
+        ax.right_ax.set_ylabel("Latency (μs)")
         fig = ax.get_figure()
         fig.savefig(dir / "byte_size.pdf")
     
@@ -55,6 +57,7 @@ class Stats(BaseModel):
             secondary_y=["average_latency"],
         )
         ax.set_ylabel("Throughput (ops/s)")
+        ax.right_ax.set_ylabel("Latency (μs)")
         fig = ax.get_figure()
         fig.savefig(dir / "db_size.pdf")
     
@@ -68,6 +71,7 @@ class Stats(BaseModel):
             secondary_y=["average_latency"],
         )
         ax.set_ylabel("Throughput (ops/s)")
+        ax.right_ax.set_ylabel("Latency (μs)")
         fig = ax.get_figure()
         fig.savefig(dir / "percent_write.pdf")
 
