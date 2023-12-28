e5ba57c592bcdd7e13df3198939d9a133f4e826a
Adrian Davila <a4davila@uwaterloo.ca>
2023-12-28 14:57:52 -0500

    format files

--- src/host/host.cpp
+++ src/host/host.cpp
@@ -10,12 +10,12 @@
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/transport/TServerSocket.h>
 
-#include "constants.h"
-#include "shared.h"
-#include "errors.h"
 #include "RPC.h"
+#include "constants.h"
+#include "errors.h"
 #include "ortoa_u.h"
 #include "redis.h"
+#include "shared.h"
 #include "spdlog/spdlog.h"
 
 using namespace apache::thrift;
@@ -38,25 +38,23 @@ class RPCHandler : virtual public RPCIf {
     std::unique_ptr<StorageInterface> storage_server;
 
   public:
-    RPCHandler(): storage_server{std::make_unique<redisCli>(HOST_IP)} {}
+    RPCHandler() : storage_server{std::make_unique<redisCli>(HOST_IP)} {}
 
     static void setEnclaveArgs(int argc, char *argv[]) {
         assert(argc >= 2);
 
         char *oe_enclave_path = argv[1];
         uint32_t oe_flag = OE_ENCLAVE_FLAG_DEBUG;
-        
+
         if (check_simulate(argc, argv)) {
-            #ifdef DEBUG
+#ifdef DEBUG
             spdlog::debug("Running in simulation mode");
-            #endif
+#endif
 
             oe_flag |= OE_ENCLAVE_FLAG_SIMULATE;
         }
 
-        oe_result_t result =
-            oe_create_ortoa_enclave(oe_enclave_path, OE_ENCLAVE_TYPE_SGX,
-                                    oe_flag, NULL, 0, &enclave);
+        oe_result_t result = oe_create_ortoa_enclave(oe_enclave_path, OE_ENCLAVE_TYPE_SGX, oe_flag, NULL, 0, &enclave);
         if (result != OE_OK) {
             throw OECreationFailed(oe_enclave_path);
         }
@@ -68,22 +66,19 @@ class RPCHandler : virtual public RPCIf {
         std::unique_ptr<unsigned char> out(new unsigned char[4096]);
         size_t out_len;
 
-        oe_result_t result =
-            access_data(enclave, operation.op, rd_value.c_str(),
-                        rd_value.length(), operation.value.c_str(),
-                        operation.value.length(), out.get(), &out_len);
+        oe_result_t result = access_data(enclave, operation.op, rd_value.c_str(), rd_value.length(),
+                                         operation.value.c_str(), operation.value.length(), out.get(), &out_len);
 
         if (result == OE_OK) {
             std::string updated_val((const char *)out.get(), out_len);
 
-            #ifdef DEBUG
-            spdlog::debug("Host | Output of access_data , {0} with len {1}", 
-                          updated_val, out_len);
-            #endif
+#ifdef DEBUG
+            spdlog::debug("Host | Output of access_data , {0} with len {1}", updated_val, out_len);
+#endif
 
             storage_server->put(operation.key, updated_val);
             _return = updated_val;
-        } 
+        }
     }
 };
 
@@ -97,17 +92,14 @@ int main(int argc, char *argv[]) {
         auto transportFactory = std::make_shared<TBufferedTransportFactory>();
         auto protocolFactory = std::make_shared<TBinaryProtocolFactory>();
 
-        std::shared_ptr<ThreadFactory> threadFactory = 
-            std::shared_ptr<ThreadFactory>(new ThreadFactory());
-        std::shared_ptr<ThreadManager> threadManager = 
-            ThreadManager::newSimpleThreadManager(16);
+        std::shared_ptr<ThreadFactory> threadFactory = std::shared_ptr<ThreadFactory>(new ThreadFactory());
+        std::shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager(16);
         threadManager->threadFactory(threadFactory);
         threadManager->start();
 
         std::shared_ptr<apache::thrift::server::TServer> server;
-        server.reset(new TThreadPoolServer(processor, serverTransport, 
-                                           transportFactory, protocolFactory, 
-                                           threadManager));
+        server.reset(
+            new TThreadPoolServer(processor, serverTransport, transportFactory, protocolFactory, threadManager));
         server->serve();
     } catch (OECreationFailed err) {
         spdlog::error("Host | {0}", err.what());
